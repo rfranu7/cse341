58bb3502c51c1e1360ad8a7046fcc795769ecaf2
rfranu7 <57077484+rfranu7@users.noreply.github.com>
2021-01-12 11:56:57 +0800

    added coming soon for assignments

---
+++ web/pages/attributions.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    
+    <h1>Resources and Attributions</h1>
+    <ul>
+        <li><a href='https://www.freepik.com/vectors/background'>Coming Soon Background vector created by starline - www.freepik.com</a></li>
+    </ul>
+
+</body>
+</html>
