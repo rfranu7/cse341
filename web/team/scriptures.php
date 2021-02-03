990a7588ec5492a459c55a81bedcbbd691373475
rfranu7 <57077484+rfranu7@users.noreply.github.com>
2021-02-03 10:30:52 +0800

    scriptures.php

---
+++ web/team/scriptures.php
@@ -0,0 +1,35 @@
+<?php 
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+$sql = "SELECT *
+        FROM public.scriptures";
+
+$statement = $db->query($sql);
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($results);
+
+
+
+?>
