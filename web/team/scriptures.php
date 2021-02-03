a033e987d9bc38e5c2a7d2fc18f27a8d41e2b3e7
rfranu7 <57077484+rfranu7@users.noreply.github.com>
2021-02-03 10:42:37 +0800

    scriptures

--- web/team/scriptures.php
+++ web/team/scriptures.php
@@ -22,21 +22,27 @@ catch (PDOException $ex)
   die();
 }
 
-$sql = "SELECT *
-        FROM public.scriptures";
+$sql = "SELECT * FROM public.scriptures";
+if(isset($_POST['book'])) {
+    $sql .= " WHERE book = $_POST[book]";
+}
+
+echo $sql;
+
 
 $statement = $db->query($sql);
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 // print_r($results);
+echo '<h1>Scripture Resources</h1>';
+echo '<form method="POST" action="./">';
+echo '<input type="text" name="book" id="book">';
+echo '<input type="submit" value="SEARCH"></form>';
 foreach($results as $row){
-    echo '<div class="scripture-block"><span class="book">'.$row['book'].' </span>';
-    echo '<span class="chapter">'.$row['chapter'].':</span>';
-    echo '<span class="verse">'.$row['verse'].'</span>';   
-    echo '<span class="content"> - "'.$row['content'].'"</span></div>';    
+    echo '<div class="scripture-block" style="margin-bottom: 1rem;"><strong class="book">'.$row['book'].' </strong>';
+    echo '<strong class="chapter">'.$row['chapter'].':</strong>';
+    echo '<strong class="verse">'.$row['verse'].'</strong>';   
+    echo '<span class="content"> - "'.$row['content'].'"</span></div>'; 
 }
-
-
-
 
 ?>
